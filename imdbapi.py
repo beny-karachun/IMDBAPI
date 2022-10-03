69c4bee776237092b48828f17dd2006f375f936b
beny-karachun <93853386+beny-karachun@users.noreply.github.com>
2022-10-03 16:54:11 +0300

    Update imdbapi.py

--- imdbapi.py
+++ imdbapi.py
@@ -1,58 +1,55 @@
-from multiprocessing.connection import wait
 import requests
 import json
 import streamlit as st
-def getRating(id):
- url = "https://imdb8.p.rapidapi.com/title/get-ratings"
+from deep_translator import GoogleTranslator 
 
- querystring = {"tconst":id}
-
- headers = {
-		"X-RapidAPI-Key": "8a67def28cmsh0bf8c467709ccdfp1df2ddjsncc7b6e252b1d",
-		"X-RapidAPI-Host": "imdb8.p.rapidapi.com"
-	}
-
- response = requests.request("GET", url, headers=headers, params=querystring)
- data = json.loads(response.text)
-
- return (data["rating"])
 
 
 def getMovieID(movieName):
+    url = "https://imdb8.p.rapidapi.com/title/find"
+
+    querystring1 = {"q":movieName}
+
+    headers = {
+        "X-RapidAPI-Key": "8a67def28cmsh0bf8c467709ccdfp1df2ddjsncc7b6e252b1d",
+        "X-RapidAPI-Host": "imdb8.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring1)
+    data = json.loads(response.text)
+    # a = (data["results"])
+    # a = json.dumps(data["results"])
+    a = data["results"][0]["id"]
+    return(a.replace("/","").replace("title",""))
 
 
- url = "https://imdb8.p.rapidapi.com/title/find"
 
- querystring1 = {"q":movieName}
+def getData(id):
+    url = "https://imdb8.p.rapidapi.com/title/get-overview-details"
 
- headers = {
-		"X-RapidAPI-Key": "8a67def28cmsh0bf8c467709ccdfp1df2ddjsncc7b6e252b1d",
-		"X-RapidAPI-Host": "imdb8.p.rapidapi.com"
-	}
+    querystring = {"tconst":id}
 
- response = requests.request("GET", url, headers=headers, params=querystring1)
- data = json.loads(response.text)
- a = json.dumps(data["results"])
- a = a[9:26]
- 
- return(a.replace("/","").replace("title",""))
- 
+    headers = {
+        "X-RapidAPI-Key": "8a67def28cmsh0bf8c467709ccdfp1df2ddjsncc7b6e252b1d",
+        "X-RapidAPI-Host": "imdb8.p.rapidapi.com"
+    }
 
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    data = json.loads(response.text)
+    return data
 
-def getDuration(ID):
- url = "https://imdb8.p.rapidapi.com/title/get-details"
+def getDuration(data):
+    return (data["title"])["runningTimeInMinutes"]
 
- querystring = {"tconst":ID}
+def getRelease(data):
+    return (data["releaseDate"])
 
- headers = {
-	"X-RapidAPI-Key": "8a67def28cmsh0bf8c467709ccdfp1df2ddjsncc7b6e252b1d",
-	"X-RapidAPI-Host": "imdb8.p.rapidapi.com"
- }
+def getRating(data):
+    return data["ratings"]
 
- response = requests.request("GET", url, headers=headers, params=querystring)
- data = json.loads(response.text)
- return(data["runningTimeInMinutes"])
-
+def getGenres(data):
+    return data["genres"]
+   
 
 
 with st.container():
@@ -69,6 +66,18 @@ with st.container():
             st.error("Your input isn't a valid movie name, please fix your input.")
     if submit and not(name):
         st.error("Please fill out the name field!")
+
+
+
+
     
     
-    
+
+
+
+# data = getData(getMovieID("Hobbit"))
+# print("Rating: " + str(getRating(data)["rating"]))
+# print("Duration: " + str(getDuration(data)))
+# print("Release date: " + str(getRelease(data))[:4])
+# print("Genres: " + GoogleTranslator(source='auto', target='de').translate(",".join(getGenres(data))))
+        # return(a.replace("/","").replace("title",""))
